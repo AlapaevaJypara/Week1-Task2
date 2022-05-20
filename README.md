11cbb6bf0993ad5d3b09c8e49b874a2e1b37fc4d
Jypara Alapaeva <jypara.alapaeva.kk@gmail.com>
2022-05-20 17:16:46 +0600

    added change

--- README.md
+++
@@ -1,2 +0,0 @@
-# Week1-Task2
-https://alapaevajypara.github.io/Week1-Task2/
--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,6 @@
 //1.Дана строка 'ddd@bbb@ccc'. Замените все @ на '!'
-let string = "ddd@bbb@ccc";
-console.log(string.replaceAll("@", "!"));
+// let string = "ddd@bbb@ccc";
+// console.log(string.replaceAll("@", "!"));
 
 //2.Дана строка 'js'. Сделайте из нее строку 'JS'.
 // let string1 = "js";
@@ -16,8 +16,8 @@ console.log(string.replaceAll("@", "!"));
 
 //5.Составить скрипт, который запрашивает у пользователя ввести номер кредитной карты. Скрипт должен обработать полученный номер и показать на странице последние 4 цифры а остальные цифры карты должны заменены на *.
 
-// let cardNum = prompt("Введите номер вашей карты?");
-// document.write(
-//   "<h1>Номер вашей карты: </h1>" +
-//     cardNum.replace(cardNum.slice(0, -4), "********")
-// );
+let cardNum = prompt("Введите номер вашей карты?");
+document.write(
+  "<h1>Номер вашей карты: </h1>" +
+    cardNum.replace(cardNum.slice(0, -4), "********")
+);
